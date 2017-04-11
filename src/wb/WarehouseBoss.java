a49a6b83e63a678eb2f69388f95a55fd4b184054
Jashank Jeremy <jashankj@cse.unsw.edu.au>
2017-04-11 19:13:57 +1000

    src: get some directory structure happening.

---
+++ src/wb/WarehouseBoss.java
@@ -0,0 +1,11 @@
+package wb;
+
+/**
+ * @brief The main entry point for the Warehouse Boss game
+ * @date 2017-04-11
+ */
+public class WarehouseBoss {
+	public static void main (Static[] args) {
+		// TODO let's get some code!
+	}
+}
