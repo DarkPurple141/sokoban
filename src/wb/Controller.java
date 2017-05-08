4ec411d81325dd5d4e0be54c32d1b19eb2b8c649
Jashank Jeremy <jashankj@cse.unsw.edu.au>
2017-05-08 22:49:26 +1000

    ctrl: Tabify and make compile.
    Signed-off-by: Jashank Jeremy <jashankj@cse.unsw.edu.au>

--- src/wb/Controller.java
+++ src/wb/Controller.java
@@ -1,62 +1,66 @@
 package wb;
 
+import java.awt.Event;
+
 class Controller {
 
-    private View v;
-    private Model m;
+	private View v;
+	private Model m;
 
-    public Controller(){
-        v = new View();
-        m = new Model();
-        // this.makeModel(filePath)
-    }
+	public Controller() {
+		v = new View();
+		m = new Model();
+		// this.makeModel(filePath)
+	}
 
-    public void makeModel(String filePath){
-    	m = new Model(filePath);
-    }
+	public void makeModel(String filePath) {
+		m = new Model(filePath);
+	}
 
-    /// this is pseudo java
-    public void run() {
-        while (true) {
-            if (gameOver) { // check current games state
-                break;
-            }
-            for (Event e : View) { // this is pseudo code
-                // update game board.
-                if (processEvent(e)) {
-                    v.render();
-                }
-                if (gameOver) {
-                    break;
-                }
-            }
-        }
-    }
+	/// this is pseudo java
+	public void run() {
+		/*
+		while (true) {
+			if (gameOver) { // check current games state
+				break;
+			}
+			for (Event e : View) { // this is pseudo code
+				// update game board.
+				if (processEvent(e)) {
+					v.render();
+				}
+				if (gameOver) {
+					break;
+				}
+			}
+		}
+		*/
+	}
 
-    // What's the view said has just happened?
-    // Update model to new state.
-    // directions in clockwise form.
-    // 0, 1, 2, 3 corresponding UP, RIGHT, DOWN, LEFT
-    private boolean processEvent(Event e) {
-        switch (e.getKeyCode()) {
-            boolean updateMade = false;
-            case (KeyEvent.VK_KP_UP):
-                updateMade = m.makeMove(0);
-                break;
-            case (KeyEvent.VK_KP_RIGHT):
-                updateMade = m.makeMove(1);
-                break;
-            case (KeyEvent.VK_KP_DOWN):
-                updateMade = m.makeMove(2);
-                break;
-            case (KeyEvent.VK_KP_LEFT):
-                updateMade = m.makeMove(3);
-                break;
-            default:
-                break;
-        }
-        return updateMade;
-    }
+	/*
+	 * What's the view said has just happened?
+	 * Update model to new state.
+	 * directions in clockwise form.
+	 * 0, 1, 2, 3 corresponding UP, RIGHT, DOWN, LEFT
+	 */
+	private boolean processEvent(Event e) {
+		/*
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_KP_UP:
+			return m.makeMove(0);
 
+		case KeyEvent.VK_KP_RIGHT:
+			return m.makeMove(1);
 
+		case KeyEvent.VK_KP_DOWN:
+			return m.makeMove(2);
+
+		case KeyEvent.VK_KP_LEFT:
+			return m.makeMove(3);
+		}
+		*/
+
+		/* FIXME jashankj: why is this `false`? */
+		return false;
+	}
 }
