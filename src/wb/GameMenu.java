635964accf905b94dae8dff3380a6707b9b4374d
Jashank Jeremy <jashankj@cse.unsw.edu.au>
2017-05-26 21:07:15 +1000

    GameMenu: sync UML; logic cleanup.
    Signed-off-by: Jashank Jeremy <jashankj@cse.unsw.edu.au>

--- src/wb/GameMenu.java
+++ src/wb/GameMenu.java
@@ -17,24 +17,28 @@ import javax.swing.border.EmptyBorder;
  */
 class GameMenu
 extends JPanel {
-	// FIXME(jashankj): this class name is quite bad
-	// FIXME(jashankj): variable names
 	private JButton playNow;
 	private JButton loadGame;
 	private JButton campaign;
 	private JButton settings;
 	private JButton exit;
 
-
 	public GameMenu(Controller c) {
 		super();
 		this.setLayout(new GridLayout(0,1,10,10));
 		this.setBorder(new EmptyBorder(80,140,20,140));
+
 		JLabel title = new JLabel("WAREHOUSE BOSS",JLabel.CENTER);
 		title.setFont(new Font("Cardinal",Font.BOLD,36));
 		title.setForeground(Color.white);
 		this.add(title);
-		this.makeButtons(c);
+
+		this.add(this.playNow = makeButton("Play Now", c));
+		this.add(this.loadGame = makeButton("Load Game", c));
+		this.add(this.campaign = makeButton("Campaign", c));
+		this.add(this.settings = makeButton("Settings", c));
+		this.add(this.exit = makeButton("Exit", c));
+
 		this.setBackground(Color.BLACK);
 		this.setFocusable(true);
 		this.setVisible(true);
@@ -60,27 +64,8 @@ extends JPanel {
 		return this.loadGame;
 	}
 
-
 	// NOTE methods below this line are private
 	// ----------------------------------
-	/**
-	 * Helper function to make all the buttons required for the Menu
-	 *
-	 * @param c		The controller which acts as the button listener.
-	 */
-	private void makeButtons(Controller c) {
-		playNow = makeButton("Play Now",c);
-		loadGame = makeButton("Load Game", c);
-		campaign = makeButton("Campaign",c);
-		settings = makeButton("Settings",c);
-		exit = makeButton("Exit",c);
-		this.add(playNow);
-		this.add(loadGame);
-		this.add(campaign);
-		this.add(settings);
-		this.add(exit);
-	}
-
 	/**
 	 * Helper function to make a new JButton for the Menu.
 	 *
@@ -89,12 +74,10 @@ extends JPanel {
 	 * @return		The new button.
 	 */
 	private JButton makeButton(String name, Controller c) {
-		JButton n;
-		n = new JButton(name);
+		JButton n = new JButton(name);
 		Insets margin = new Insets(20,20,20,20);
 		n.setMargin(margin);
 		n.addActionListener(c);
 		return n;
 	}
-
 }
